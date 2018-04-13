bf3554002cb12b2cf91e46a7b501b865a83fb785
Richard Cho <richardcho100@gmail.com>
2018-04-13 21:19:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
-# Add  code here!
+require "pry"
+
+# All primes except 2 are odd
+# All primes greater than 3 can be written in the form 6k - 1 or 6k + 1.
+# You don't need to check past the square root of n
+
+def prime?(n)
+  # if n > 0
+  #   n % 2 == 0 || n % 3 == 0
+  # end
+
+  if n > 0
+    arr = (1..n).to_a
+    arr.select! do |number|
+      n % number == 0
+    end
+  end
+end
+
+array = []
+if n == 2
+  true
+elsif n > 2 && n.odd?
+  array << n
